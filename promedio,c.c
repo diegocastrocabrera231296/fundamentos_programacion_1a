8bab4fd14f647bec1f50106828b4447e6c016830
diegocastrocabrera231296 <diego961223@gmail.com>
2015-11-13 07:44:06 -0600

    promedio

---
+++ promedio,c.c
@@ -0,0 +1,68 @@
+#include<stdio.h> 
+int n1;
+int n2; 
+int n3; 
+int n4;
+int n5;
+int n6;
+int n7;
+int n8;
+int n9;
+int n10;
+int arreglo[10];
+int promedio;
+int main()
+{//inicio main 
+{
+printf("dame la primera calificacion");
+arreglo[0]=n1;
+scanf("%d",&n1);
+}
+{
+printf("dame la segunda calificacion");
+arreglo[1]=n2;
+scanf("%d",&n2);
+}
+{
+printf("dame la tercera calificacion");
+arreglo[2]=n3;
+scanf("%d",&n3);
+}
+{
+printf("dame la cuarta calificacion");
+arreglo[3]=n4;
+scanf("%d",&n4);
+}
+{
+printf("dame la quinta calificacion");
+arreglo[4]=n5;
+scanf("%d",&n5);
+}
+{
+printf("dame la sexta calificacion");
+arreglo[5]=n6;
+scanf("%d",&n6);
+}
+{
+printf("dame la septima calificacion");
+arreglo[6]=n7;
+scanf("%d",&n7);
+}
+{
+printf("dame la octava calificacion");
+arreglo[7]=n8;
+scanf("%d",&n8);
+}
+{
+printf("dame la novena calificacion");
+arreglo[8]=n9;
+scanf("%d",&n9);
+}
+{
+printf("dame la decima calificacion");
+arreglo[9]=n10;
+scanf("%d",&n10);
+}
+    promedio=n1+n2+n3+n4+n5+n6+n7+n8+n9+n10/10;
+	printf("elpromedio es:%d",promedio);
+}//fin main
